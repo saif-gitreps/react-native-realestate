628dff0b8da6b9e0b4c985ac2c0fb49f4f202a82
Saif <droconos99@gmail.com>
2026-08-28 16:46:40 +0600

    Update major stuffs

---
+++ PERSONAL_NOTES.md
@@ -0,0 +1,15 @@
+- npx create-expo-app@latest --template default@sdk-54 .
+- then u can run reset to clear all the boilerplate code
+- to start npx expo start
+- error while running npx expo start, use npx expo start -- --port=8080 instead, got it from reddit.
+- if you want to create tabs navigation use (tabs) file and keep all the stuffs over there.
+
+- quick cheatsheet on the diff between DOM and Native:
+- ![alt text](image.png)
+
+- texts related stuffs must always be wrapped in a <Text> Text </Text> component
+- to built for notch phones, use
+- <TouchableOpacity> as button, and here we have onPress instead of onClick
+
+- cheatsheet for key components
+  ![alt text](image-1.png)
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,54 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { FlatList, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
+type Rooms = {
+   id: number;
+   name?: string;
+   descriptions?: string;
 };
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+   const rooms: Rooms[] = [
+      { id: 1, name: "2 bed", descriptions: "nice room" },
+      { id: 2, name: "3 bed", descriptions: "nice room 2" },
+   ];
+   return (
+      <SafeAreaView style={{ flex: 1, borderRadius: 10 }}>
+         <View
+            style={{
+               padding: 16,
+               margin: 30,
+               backgroundColor: "#bebebe",
+               flex: 1,
+            }}
+         >
+            <Text>Hello</Text>
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+            <TextInput
+               placeholder="hello"
+               style={{ padding: 14, marginTop: 5, borderColor: "#c28f8f" }}
+            ></TextInput>
+
+            <TouchableOpacity
+               style={{ backgroundColor: "#1e1a1a", borderRadius: 8, padding: 10 }}
+            >
+               <Text style={{ color: "#fff" }}>Search</Text>
+            </TouchableOpacity>
+
+            <FlatList
+               data={rooms}
+               keyExtractor={(room) => String(room.id)}
+               renderItem={({ item }) => (
+                  <View
+                     style={{ flexDirection: "row", padding: 10, margin: 10, gap: 10 }}
+                  >
+                     <Text>{item.id}</Text>
+                     <Text>{item.name}</Text>
+                     <Text>{item.descriptions}</Text>
+                  </View>
+               )}
+            />
+         </View>
+      </SafeAreaView>
+   );
 }
